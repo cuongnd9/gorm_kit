c941148d722ad1b3d209ba232bea8e5984192823
Cuong Tran <103cuong@gmail.com>
2020-09-05 22:11:36 +0700

    feat: pure model

---
+++ models/pure-model.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// Pure pure/base model
+type Pure struct {
+	ID        uuid.UUID      `gorm:"primary_key;type:uuid;default:uuid_generate_v4()" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+}
