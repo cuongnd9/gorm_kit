befaeb3e24334d844a06ac91215f65688f73de44
Cuong Tran <103cuong@gmail.com>
2020-09-06 01:31:31 +0700

    feat: env & default configs

--- configs/database.go
+++ configs/database.go
@@ -3,8 +3,9 @@ package configs
 
 import (
 	"fmt"
-
+	"github.com/mcuadros/go-defaults"
 	"gorm.io/gorm"
+	"os"
 )
 
 // DB GORM DB instance
@@ -12,29 +13,30 @@ var DB *gorm.DB
 
 // DBConfig database configuration
 type DBConfig struct {
-	Host     string
-	Port     uint
-	User     string
-	DBName   string
-	Password string
+	Host     string `default:"0.0.0.0"`
+	Port     string `default:"5432"`
+	User     string `default:"postgres"`
+	DBName   string `default:"postgres"`
+	Password string `default:"postgres"`
 }
 
 // BuildDBConfig build DB config
 func BuildDBConfig() *DBConfig {
 	dbConfig := DBConfig{
-		Host:     "0.0.0.0",
-		Port:     5432,
-		User:     "postgres",
-		DBName:   "postgres",
-		Password: "postgres",
+		Host:     os.Getenv("PG_HOST"),
+		Port:     os.Getenv("PG_PORT"),
+		User:     os.Getenv("PG_USER"),
+		DBName:   os.Getenv("PG_DBNAME"),
+		Password: os.Getenv("PG_PASSWORD"),
 	}
+	defaults.SetDefaults(&dbConfig)
 	return &dbConfig
 }
 
 // BuildDSN build Postgresql's DSN
 func BuildDSN() string {
 	dbConfig := BuildDBConfig()
-	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		dbConfig.Host,
 		dbConfig.User,
 		dbConfig.Password,
--- go.mod
+++ go.mod
@@ -10,10 +10,12 @@ require (
 	github.com/google/uuid v1.1.2
 	github.com/lib/pq v1.8.0
 	github.com/mattn/go-sqlite3 v1.14.2 // indirect
+	github.com/mcuadros/go-defaults v1.2.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pressly/goose v2.7.0-rc5+incompatible
 	github.com/ziutek/mymysql v1.5.4 // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	gopkg.in/mcuadros/go-defaults.v1 v1.1.0 // indirect
 	gorm.io/driver/postgres v1.0.0
 	gorm.io/gorm v1.20.0
 )
